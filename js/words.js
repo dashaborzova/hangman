d0e3e9b549f102850660f150cbbacf8501d3496c
dashaborzova <dashaborzova@yandex.ru>
2024-01-13 23:09:38 +0300

    feat: add secret words to guess

---
+++ js/words.js
@@ -0,0 +1,42 @@
+const words = [
+    {
+        word: "advance",
+        hint: "Progress or a development in a particular activity or area of understanding."
+    },
+    {
+        word: "algorithm",
+        hint: "A set of rules that must be followed when solving a particular problem."
+    },
+    {
+        word: "bundle",
+        hint: "A number of things tied or wrapped together."
+    },
+    {
+        word: "chatbot ",
+        hint: "A computer program that can hold a conversation with a person, usually over the internet."
+    },
+    {
+        word: "debugger",
+        hint: "A computer program that helps to find and correct mistakes in other programs."
+    },
+    {
+        word: "default",
+        hint: "What happens or appears if you do not make any other choice or change."
+    },
+    {
+        word: "firewall",
+        hint: "A part of a computer system that prevents people from reaching information without permission."
+    },
+    {
+        word: "function",
+        hint: "A part of a program, etc. that performs a basic operation."
+    },
+    {
+        word: "hackathon",
+        hint: "An event at which a large number of people work together developing computer programs."
+    },
+    {
+        word: "notification",
+        hint: "An automated message sent by an app to inform the user of a new message, update, etc."
+    }
+]
